feabb690f2c0d7ebf4d45dcebc73e64c26f84348
Gimaevazat <55442406+Gimaevazat@users.noreply.github.com>
2019-09-24 16:21:59 +0500

    e

--- Snake.py
+++ Snake.py
@@ -1,6 +1,8 @@
 from graph import *
 import random
 
+#скорость змеи в зависимости от нажатой кнопки
+#При нажатии кнопки змея поворачивает, и в это время в list заносится пара головы(это же пара поворота). Эта пара заносится сразу после пары головы
 def keyPressed(event):
   global dx, dy, V, x, y
   if event.keycode == VK_LEFT:
@@ -25,20 +27,25 @@ def keyPressed(event):
   elif event.keycode == VK_ESCAPE:
     close()
 
+#отрисовка змеи
 def doMove():
   global dx, dy, obj, x, y, V
+  #удаление старой змеи
   deleteObject(obj)
   x += dx
   y += dy
+  #установка нового положения головы
   list[0] = (x, y)
+  #i - номер конца змеи в массиве
   i = len(list) - 1
   A = list[i]
   B = list[i - 1]
-  if A[0] >= B[0] - V and A[0] <= B[0] + V and A[1] >= B[1] - V and A[1] <= B[1] + V :
+  if A[0] >= B[0] - V and A[0] <= B[0] + V and A[1] >= B[1] - V and A[1] <= B[1] + V : #если конец совпал с первым поворотом, то удаляем первый поворот
     list.pop()
     i -= 1
     A = list[i]
     B = list[i - 1]
+    #без следующих условий конец догонял голову
     if A[0] == B[0]:
       if A[1] > B[1]:
         list[i] = (A[0], A[1] + V)
@@ -50,6 +57,7 @@ def doMove():
       else:
         list[i] = (A[0] - V, A[1])
     A = list[i]
+  #движение конца в сторону первого поворота
   if A[0] == B[0]:
     if A[1] > B[1]:
       list[i] = ( A[0], A[1] - V)
@@ -59,6 +67,7 @@ def doMove():
       list[i] = (A[0] - V, A[1])
     else:
       list[i] = (A[0] + V, A[1])
+  #отрисовка новой змеи
   penColor("green")
   obj = polyline(list)
 
@@ -87,6 +96,7 @@ def eating():
   eat = rectangle(Xe, Ye, Xe + 6, Ye + 6)
   Sqore = label (sqore, 45, 5, bg="blue", fg="red")
 
+#проверка на удар об стену
 def wall ():
   global dx, dy, x, y, V
   if x < 5 and dx < 0:
@@ -102,12 +112,11 @@ def wall ():
     V = 0
     dy = 0
 
-V = 0
-L = 100
-sqore = 0
-Xe = random.randint(0,500)
+V = 0     #скорость змеи
+L = 100     #длина змеи, во время поедания увеличивается
+sqore = 0     #счёт
+Xe = random.randint(0,500)    #первая еда
 Ye = random.randint(0,600)
-print(Xe,Ye)
 penSize(5)
 penColor("green")
 brushColor("blue")
@@ -115,7 +124,7 @@ windowSize(500, 600)
 rectangle(0, 0, 500, 600)
 x = 100; y = 100
 dx = 0;  dy = 0
-list = [(x, y),(x + L , y)]
+list = [(x, y),(x + L , y)]     #list - массив, в котором хранятся пары. Пара состоит из координат. Каждая пара это либо хвост, либо голова, либо поворот
 obj = polyline(list)
 brushColor("red")
 eat = rectangle(Xe, Ye, Xe + 20, Ye + 20)
